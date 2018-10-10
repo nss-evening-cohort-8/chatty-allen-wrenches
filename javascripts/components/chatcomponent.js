bf4a2c06b496c0ed1f1cc0f6a7abc5f67a8db850
RichFisher <rich.fishernc@gmail.com>
2018-10-09 20:22:17 -0500

    made delete function

--- javascripts/components/chatcomponent.js
+++ javascripts/components/chatcomponent.js
@@ -17,7 +17,13 @@ const messagesBuilder = (messagesArray) => {
 // Rich's code starts here
 
 function deleteMessage() {
-    let deleteMe = event.target.closest('.');
+    let deleteMe = event.target.closest('.messageDiv').id;
+    for(let i = 0; i < messagesArray.length; i++) {
+        if(messagesArray[i].id === deleteMe) {
+            messagesArray.splice(i, 1);
+            break;
+        }
+    }
     messageBuilder();
 }
 
