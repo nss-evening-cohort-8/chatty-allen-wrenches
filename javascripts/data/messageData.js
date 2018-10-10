7fa66e3252b245bd25ef4324a70309776efaf7f1
Robert Rice <rrice1291@gmail.com>
2018-10-09 19:49:33 -0500

    added xhr request

---
+++ javascripts/data/messageData.js
@@ -0,0 +1,22 @@
+import{setMessages,getMessagesz,messagesBuilder} from '../components/petComponent.js'
+
+function executeThisCodeAfterFileLoaded () {
+    const data = JSON.parse(this.responseText);
+    setMessages(data.messagesArray);
+    messagesBuilder(getMessagesz());
+}
+
+function executeThisCodeIfXhrFails () {
+    console.log('it broke')
+}
+
+
+const getMessages = () => {
+    let myRequest = new XMLHttpRequest();
+    myRequest.addEventListener('load',executeThisCodeAfterFileLoaded);
+    myRequest.addEventListener('error', executeThisCodeIfXhrFails);
+    myRequest.open('GET', '../../db/messages.json');
+    myRequest.send();
+};
+
+export{getMessages};
