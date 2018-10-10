d9f199d885a04d99b692647e68ecc6a9c54b3e24
RichFisher <rich.fishernc@gmail.com>
2018-10-09 20:05:17 -0500

    merging ryans shit

--- javascripts/components/chatcomponent.js
+++ javascripts/components/chatcomponent.js
@@ -1,1 +1,10 @@
-console.log('this is chat components js')
+// Rich's code starts here
+
+import {writeToDom, addToDom} from '../helpers/util.js';
+
+function deleteMessage() {
+    let deleteMe = event.target.closest('.');
+    messageBuilder();
+}
+
+export {deleteMessage};
