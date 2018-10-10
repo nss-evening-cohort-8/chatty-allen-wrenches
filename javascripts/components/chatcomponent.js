f0f9ee42d5898122a79a0396b104581e24f5851d
RichFisher <rich.fishernc@gmail.com>
2018-10-09 21:25:20 -0500

    signing off for the night

--- javascripts/components/chatcomponent.js
+++ javascripts/components/chatcomponent.js
@@ -12,8 +12,23 @@ const messagesBuilder = (messagesArray) => {
     console.log('this is messages Builder function')
 }
 
-function editMessage() {
-    
+let editing = 0;
+
+function beginEditMessage() {
+    let editMe = event.target.closest('.messageDiv').id;
+    for(let i = 0; i < messagesArray.length; i++) {
+        if(messagesArray[i].id === editMe) {
+            document.getElementById('input').value = messagesArray[i].message;
+            editing = 1;
+            break;
+        }
 }
 
-export{getMessagesz,setMessages,messagesBuilder, editMessage};
+function saveEditMessage() {
+    if(editing === 1) {
+        messagesArray[].message = document.getElementById('input');
+        messagesArray[].message = event.timeStamp;
+    }
+}
+
+export{getMessagesz,setMessages,messagesBuilder, beginEditMessage};
