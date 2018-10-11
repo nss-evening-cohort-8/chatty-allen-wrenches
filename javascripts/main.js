46d9929fa223826f70688e207ee012f47db2777b
Austin Cumberlander <austincumberlander@gmail.com>
2018-10-10 19:53:14 -0500

    wip

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,18 +1,13 @@
 import{getMessages} from './data/messageData.js'
 import {messageSubmit, messageSubmitClick, clearMessages, makeDark, makeTextLarge} from '../javascripts/eventlisteners.js';
 
-<<<<<<< HEAD
-getMessages();
-messageSubmit();
-messageSubmitClick();
-clearMessages();
-makeDark();
-makeTextLarge();
-=======
 function initializeApp() {
     getMessages();
     messageSubmit();
+    messageSubmitClick();
+    clearMessages();
+    makeDark();
+    makeTextLarge();
 }
 
 initializeApp();
->>>>>>> master
